8f553ffe7aca5468de1defbba2206647cd226eb4
guy_nveh <guyznave@gmail.com>
2021-08-08 10:40:02 +0300

    adding NavBar component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  HashRouter
 } from "react-router-dom";
 import './App.css';
+import Navbar from './Components/Nav-cp/Navbar';
 import DairyPage from './pages/DairysPage/DairyPage';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
+   
     <HashRouter>
+       <Navbar/>
       <Switch>
         <Route exact path="/"><HomePage/></Route>
         <Route exact path="/dairys"><DairyPage/></Route>
