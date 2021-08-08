5dff555ec4b1e51ea099e42c6fc5c5816ac0ace5
guy_nveh <guyznave@gmail.com>
2021-08-08 22:13:26 +0300

    creating the model user

---
+++ src/model/UserModel.js
@@ -0,0 +1,10 @@
+class UserModel {
+    constructor (id, fname,lname, email, pwd, privelage){
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email; 
+        this.pwd = pwd;
+        this.privelage = privelage;
+    }
+}
