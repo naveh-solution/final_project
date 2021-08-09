9c18421942793d170dac6f83d4d4944683e4ecf6
guy_nveh <guyznave@gmail.com>
2021-08-09 14:47:41 +0300

    creating the first Form in the component

--- src/Components/painEvent-cp/PainEventComp.jsx
+++ src/Components/painEvent-cp/PainEventComp.jsx
@@ -5,7 +5,20 @@ import {Form} from 'react-bootstrap'
 function PainEventComp() {
     return (
         <div className="eventDiv">
-            <Form></Form>
+            <Form>
+                <Form.Group className="mb-3" controlId="formPainEventLoc">
+                    <Form.Label>Location of the Pain</Form.Label>
+                    <Form.Control as="textarea" rows={3} ></Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPainEventTrigger:">
+                    <Form.Label>Physical Trigger:</Form.Label>
+                    <Form.Control as="textarea" rows={3} ></Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPainEventStartT:">
+                    <Form.Label>Start Time:</Form.Label>
+                    <Form.Control as="textarea" rows={3} ></Form.Control>
+                </Form.Group>
+            </Form>
             
         </div>
     );
