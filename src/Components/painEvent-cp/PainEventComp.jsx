06455671414e29dbcd2ecdf6120efefefe01daa4
guy_nveh <guyznave@gmail.com>
2021-08-10 12:02:59 +0300

    adding button to create event finishing disgin

--- src/Components/painEvent-cp/PainEventComp.jsx
+++ src/Components/painEvent-cp/PainEventComp.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Form } from 'react-bootstrap'
+import { Button, Col, Form, Row } from 'react-bootstrap'
 import './painEventComp.css'
 
 
-function PainEventComp({ currentTime }) {
+function PainEventComp({ currentTime, addEvent }) {
 
     const [painLoc, setPainLoc] = React.useState("");
     const [painTriger, setPainTriger] = React.useState("");
@@ -16,34 +16,49 @@ function PainEventComp({ currentTime }) {
 
     return (
         <div className="eventDiv-cp">
-            <Form>
-            <Form.Group className="mb-3" controlId="formPainEventStartT:">
-                    <Form.Label>Date of Event</Form.Label>
-                    <Form.Control type="date"  className="textarea" value={eventDate} onChange={e => setEventDate(e.target.value)} ></Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPainEventLoc">
-                    <Form.Label>Location of the Pain</Form.Label>
-                    <Form.Control as="textarea" rows={3} className="textarea" value={painLoc} onChange={e => setPainLoc(e.target.value)} ></Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPainEventTrigger:">
-                    <Form.Label>Physical Trigger:</Form.Label>
-                    <Form.Control as="textarea" rows={3} className="textarea" value={painTriger} onChange={e => setPainTriger(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPainEventStartT:">
-                    <Form.Label>Start Time:</Form.Label>
-                    <Form.Control type="time" className="textarea" value={startT} onChange={e => setStartT(e.target.value)} ></Form.Control>
-                </Form.Group>
-            </Form>
-            <Form>
-                <Form.Group className="mb-3" controlId="formPainEventSelfT">
-                    <Form.Label>Self Thoughts:</Form.Label>
-                    <Form.Control as="textarea" rows={3} className="textarea" value={selfT} onChange={e => setSelfT(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPainEventSelfT">
-                    <Form.Label>Environment Status:</Form.Label>
-                    <Form.Control as="textarea" rows={3} className="textarea" value={envStatus} onChange={e => setEnvStatus(e.target.value)}></Form.Control>
-                </Form.Group>
-            </Form>
+            <Row>
+                <Col className="leftPenEvent">
+                    <Form>
+                        <div className="mb-3 dateTimeRow">
+
+                            <Form.Group as={Col} controlId="formPainEventStartT:">
+                                <Form.Label>Date of Event</Form.Label>
+                                <Form.Control type="date" className="dateTime" value={eventDate} onChange={e => setEventDate(e.target.value)} ></Form.Control>
+                            </Form.Group>
+
+
+                            <Form.Group as={Col} controlId="formPainEventStartT:">
+                                <Form.Label>Start Time:</Form.Label>
+                                <Form.Control type="time" className="dateTime" value={startT} onChange={e => setStartT(e.target.value)} ></Form.Control>
+                            </Form.Group>
+
+                        </div>
+                        <Form.Group className="mb-3" controlId="formPainEventLoc">
+                            <Form.Label>Location of the Pain</Form.Label>
+                            <Form.Control as="textarea" rows={3} className="textarea" value={painLoc} onChange={e => setPainLoc(e.target.value)} ></Form.Control>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPainEventTrigger:">
+                            <Form.Label>Physical Trigger:</Form.Label>
+                            <Form.Control as="textarea" rows={3} className="textarea" value={painTriger} onChange={e => setPainTriger(e.target.value)}></Form.Control>
+                        </Form.Group>
+                    </Form>
+                </Col>
+                <Col className="rightPenEvent">
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formPainEventSelfT">
+                            <Form.Label>Self Thoughts:</Form.Label>
+                            <Form.Control as="textarea" rows={3} className="textarea" value={selfT} onChange={e => setSelfT(e.target.value)}></Form.Control>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPainEventSelfT">
+                            <Form.Label>Environment Status:</Form.Label>
+                            <Form.Control as="textarea" rows={3} className="textarea" value={envStatus} onChange={e => setEnvStatus(e.target.value)}></Form.Control>
+                        </Form.Group>
+                    </Form>
+                </Col>
+                <Row>
+                    <Button className="createBtn" onClick={addEvent}>Create</Button>
+                </Row>
+            </Row>
         </div>
     );
 }
