49d5c8a86cc55cb49059ca7dba85a5c4a39b414f
guy_nveh <guyznave@gmail.com>
2021-08-16 10:35:57 +0300

    home-page disign

--- src/Components/cardHome-cp/CardComp.jsx
+++ src/Components/cardHome-cp/CardComp.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
 import './cardComp.css'
 
-function CardComp() {
+function CardComp({imgSrc, titel, text}) {
     return (
         <div className="cardDiv">
-            <h5>Physical</h5>
-            <p>The physical pain is reale and you can feel it some where in your body</p>
-            
-            {/* <Card>
-                <Card.Img variant="top" src="https://image.flaticon.com/icons/png/512/387/387630.png" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card> */}
+            <img className="cardImg" src={imgSrc} alt="headace" />
+            <h5>{titel}</h5>
+            <p>{text}</p>
         </div>
     );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -15,9 +15,9 @@ function HomePage({ users, login, userActive }) {
         <div className="home-p">
             <h1>Pain Diary</h1>
             <div className="cardsDiv">
-                <CardComp />
-                <CardComp />
-                <CardComp />
+                <CardComp imgSrc="https://static.thenounproject.com/png/3968708-200.png" titel="Physical" text="The physical pain is reale and you can feel it some where in your body" />
+                <CardComp imgSrc="https://i.dlpng.com/static/png/6331675_preview.png" titel="Psychological" text="We are what we think our thogut affect our life"/>
+                <CardComp imgSrc="https://image.flaticon.com/icons/png/128/2821/2821176.png" titel="Environmental" text="How the Environment is reacting is part of what we think about our self"/>
             </div>
             <div className="login-div">
                 {!userActive ? <LoginComp users={users} login={login} userActive={userActive} /> : null}
